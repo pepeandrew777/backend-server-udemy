7aef2fde1082b8a85e16cd522d82e878d8e0dfd5
villaa <villaa@epsas.com.bo>
2019-07-29 10:54:34 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 //Inicializar variables
 var app = express();
 
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+//Rutas
+app.use('/',appRoutes);
+app.use('/usuario',usuarioRoutes);
+app.use('/login',loginRoutes);
 
 // Conexion a la base de datos
 
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err,res)=>{
    if(err) throw err;
    console.log('Base de datos:\x1b[32m%s\x1b[0m','online');
-});
-//Rutas
-app.get('/',(req,res,next)=>{
-
-
-    res.status(200).json({
-        ok:true,
-        mensaje:'Petición realizada correctamente'
-    });
 });
 
 
